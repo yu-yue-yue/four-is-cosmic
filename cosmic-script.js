1f20e5623d3fdeec8b163b91c43c3afa3c84746a
Evelyn Yu <156254701+yu-yue-yue@users.noreply.github.com>
2024-06-02 18:14:38 -0400

    Update cosmic-script.js

--- cosmic-script.js
+++ cosmic-script.js
@@ -1,4 +1,37 @@
-function displayNumber() {
+function getCosmicSequence() {
     var number = document.getElementById('numberInput').value;
     document.getElementById('displayArea').innerText = number;
+    var arr = [number];
+    while (number != 4) {
+        number = convertToWords(number).length;
+        arr.push(number); 
+    }
+
+    return arr;
 }
+
+
+const units = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+const teens = ["", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+const tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+const scales = ["", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion"];
+
+function convertToWords(num) {
+    if (num === 0) return "Zero";
+    return convert(num).trim();
+}
+
+function convert(num) {
+    if (num < 10) return units[num];
+    if (num < 20) return teens[num - 10];
+    if (num < 100) return tens[Math.floor(num / 10)] + (num % 10 !== 0 ? " " + units[num % 10] : "");
+    if (num < 1000) return units[Math.floor(num / 100)] + " Hundred" + (num % 100 !== 0 ? " " + convert(num % 100) : "");
+    
+    for (let i = 1; i < scales.length; i++) {
+        let unitValue = 1000 ** i;
+        if (num < unitValue * 1000) {
+            return convert(Math.floor(num / unitValue)) + " " + scales[i] + (num % unitValue !== 0 ? " " + convert(num % unitValue) : "");
+        }
+    }
+}
+
